c44e058db3de5f1d3d814c098b684712d4f3b0c2
robert-juru <99185183+robert-juru@users.noreply.github.com>
2023-08-10 14:46:22 +0300

    Implement winner detection

--- script.js
+++ script.js
@@ -7,7 +7,43 @@ const Gameboard = (() => {
     return { table };
 })();
 
+const detectWinner = () => {
+    let winner;
 
+    for (let i = 0; i < 3; i++) { // for Player 1(X)
+        if ((Gameboard.table[i][0] === 'X' && Gameboard.table[i][1] === 'X' && Gameboard.table[i][2] === 'X') || // row check 
+            (Gameboard.table[0][i] === 'X' && Gameboard.table[1][i] === 'X' && Gameboard.table[2][i] === 'X') || // column check
+            (Gameboard.table[0][0] === 'X' && Gameboard.table[1][1] === 'X' && Gameboard.table[2][2] === 'X') || // diagonal check
+            (Gameboard.table[0][2] === 'X' && Gameboard.table[1][1] === 'X' && Gameboard.table[2][0] === 'X')) {
+            console.log('Player 1 WON!')
+            winner = 'Player 1';
+        }
+    }
+
+    for (let i = 0; i < 3; i++) {  // for Player 2(O)
+        if ((Gameboard.table[i][0] === 'O' && Gameboard.table[i][1] === 'O' && Gameboard.table[i][2] === 'O') || // row check 
+            (Gameboard.table[0][i] === 'O' && Gameboard.table[1][i] === 'O' && Gameboard.table[2][i] === 'O') || // column check
+            (Gameboard.table[0][0] === 'O' && Gameboard.table[1][1] === 'O' && Gameboard.table[2][2] === 'O') || // diagonal check
+            (Gameboard.table[0][2] === 'O' && Gameboard.table[1][1] === 'O' && Gameboard.table[2][0] === 'O')) {
+            console.log('Player 2 WON!')
+            winner = 'Player 2'
+        }
+    }
+
+    let allCellsFilled = true; 
+    for (let i = 0; i < 3; i++) { // for draw
+        for (let j = 0; j < 3; j++) {
+            if (Gameboard.table[i][j] === '') {
+                allCellsFilled = false;
+                break;
+            }
+        }
+    }
+    if (allCellsFilled) {
+        console.log('draw!')
+        winner = 'draw';
+    }
+}
 
 const DisplayController = (() => {
     const cells = document.querySelectorAll('.cell');
