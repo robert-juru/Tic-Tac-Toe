b66a2210c6375ec76bddc006db4f319ee5a007e1
robert-juru <99185183+robert-juru@users.noreply.github.com>
2023-08-10 19:44:27 +0300

    Implemented game result handler and added board reset functionality

--- script.js
+++ script.js
@@ -7,48 +7,72 @@ const Gameboard = (() => {
     return { table };
 })();
 
-const detectWinner = () => {
-    let winner;
-
-    for (let i = 0; i < 3; i++) { // for Player 1(X)
-        if ((Gameboard.table[i][0] === 'X' && Gameboard.table[i][1] === 'X' && Gameboard.table[i][2] === 'X') || // row check 
-            (Gameboard.table[0][i] === 'X' && Gameboard.table[1][i] === 'X' && Gameboard.table[2][i] === 'X') || // column check
-            (Gameboard.table[0][0] === 'X' && Gameboard.table[1][1] === 'X' && Gameboard.table[2][2] === 'X') || // diagonal check
-            (Gameboard.table[0][2] === 'X' && Gameboard.table[1][1] === 'X' && Gameboard.table[2][0] === 'X')) {
-            console.log('Player 1 WON!')
-            winner = 'Player 1';
-        }
-    }
-
-    for (let i = 0; i < 3; i++) {  // for Player 2(O)
-        if ((Gameboard.table[i][0] === 'O' && Gameboard.table[i][1] === 'O' && Gameboard.table[i][2] === 'O') || // row check 
-            (Gameboard.table[0][i] === 'O' && Gameboard.table[1][i] === 'O' && Gameboard.table[2][i] === 'O') || // column check
-            (Gameboard.table[0][0] === 'O' && Gameboard.table[1][1] === 'O' && Gameboard.table[2][2] === 'O') || // diagonal check
-            (Gameboard.table[0][2] === 'O' && Gameboard.table[1][1] === 'O' && Gameboard.table[2][0] === 'O')) {
-            console.log('Player 2 WON!')
-            winner = 'Player 2'
-        }
-    }
-
-    let allCellsFilled = true; 
-    for (let i = 0; i < 3; i++) { // for draw
-        for (let j = 0; j < 3; j++) {
-            if (Gameboard.table[i][j] === '') {
-                allCellsFilled = false;
-                break;
+const DetectResult = (() => {
+    let winner = null;
+    const checkWinner = () => {
+        for (let i = 0; i < 3; i++) { // for Player 1(X)
+            if ((Gameboard.table[i][0] === 'X' && Gameboard.table[i][1] === 'X' && Gameboard.table[i][2] === 'X') || // row check 
+                (Gameboard.table[0][i] === 'X' && Gameboard.table[1][i] === 'X' && Gameboard.table[2][i] === 'X') || // column check
+                (Gameboard.table[0][0] === 'X' && Gameboard.table[1][1] === 'X' && Gameboard.table[2][2] === 'X') || // diagonal check
+                (Gameboard.table[0][2] === 'X' && Gameboard.table[1][1] === 'X' && Gameboard.table[2][0] === 'X')) {
+                winner = 'Player 1';
             }
         }
+        for (let i = 0; i < 3; i++) {  // for Player 2(O)
+            if ((Gameboard.table[i][0] === 'O' && Gameboard.table[i][1] === 'O' && Gameboard.table[i][2] === 'O') || // row check 
+                (Gameboard.table[0][i] === 'O' && Gameboard.table[1][i] === 'O' && Gameboard.table[2][i] === 'O') || // column check
+                (Gameboard.table[0][0] === 'O' && Gameboard.table[1][1] === 'O' && Gameboard.table[2][2] === 'O') || // diagonal check
+                (Gameboard.table[0][2] === 'O' && Gameboard.table[1][1] === 'O' && Gameboard.table[2][0] === 'O')) {
+                winner = 'Player 2';
+            }
+        }
+        return winner;
     }
-    if (allCellsFilled) {
-        console.log('draw!')
-        winner = 'draw';
+    const checkDraw = () => {
+        let allCellsFilled = true;
+        for (let i = 0; i < 3; i++) { // for draw
+            for (let j = 0; j < 3; j++) {
+                if (Gameboard.table[i][j] === '') {
+                    allCellsFilled = false;
+                    break;
+                }
+            }
+        }
+        if (allCellsFilled) {
+            console.log('draw!')
+            return true;
+        }
+        return false;
     }
-}
+    return { checkWinner, checkDraw }
+})()
+
+const handleGameResult = () => {
+    let resultMessage = document.querySelector('.result-message')
+    const result = DetectResult.checkWinner();
+    const cells = DisplayController.cells;
+    if (result === 'Player 1') {
+        resultMessage.textContent = 'Player 1 Won!'
+    } else if (result === 'Player 2') {
+        resultMessage.textContent = 'Player 2 Won!'
+
+    } else if (DetectResult.checkDraw()) {
+        resultMessage.textContent = 'Draw!'
+    }
+    //board resetter
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            Gameboard.table[i][j] = '';
+            cells.forEach(cell => {
+                cell.textContent = ''
+            })
+        }
+    }
+};
 
 const DisplayController = (() => {
     const cells = document.querySelectorAll('.cell');
     let nextSymbol = '';
-
     cells.forEach(cell => {
         const row = parseInt(cell.getAttribute('data-row'));
         const col = parseInt(cell.getAttribute('data-column'));
@@ -63,10 +87,22 @@ const DisplayController = (() => {
                 cell.textContent = Gameboard.table[row][col];
                 nextSymbol = 'X';
             }
-            detectWinner()
+            if (DetectResult.checkWinner() || DetectResult.checkDraw()) {
+                handleGameResult();
+            }
         });
     });
+    return { cells }
 })()
+
+
+
+
+
+
+
+
+
 
 
 
