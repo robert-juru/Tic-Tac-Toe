f193704948066a88e1dade56feeeb87024b4b25d
robert-juru <99185183+robert-juru@users.noreply.github.com>
2023-08-11 20:45:44 +0300

    Implemented JavaScript functionality for modal box

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 let gameEnded = false;
+let player1NameInput;
+let player2NameInput;
+
+const initializeModal = (() => {
+    document.addEventListener("DOMContentLoaded", () => {
+        const modal = document.getElementById("myModal");
+        const submitButton = document.getElementById("submitNames");
+        const displayPlayer1Name = document.getElementById("displayPlayer1Name");
+        player1NameInput = document.getElementById("player1Name");
+        player2NameInput = document.getElementById("player2Name");
+        modal.style.display = "block";
+
+        submitButton.addEventListener("click", () => {
+            const player1Name = player1NameInput.value;
+            const player2Name = player2NameInput.value;
+            displayPlayer1Name.textContent = player1Name + ": X";
+            displayPlayer2Name.textContent = player2Name + ": O";
+            displayPlayer1Name.style.fontSize = "24px"
+            displayPlayer2Name.style.fontSize = "24px"
+            modal.style.display = "none";
+        });
+    });
+})()
+
 const Gameboard = (() => {
     const table = [
         [' ', ' ', ' '],
@@ -16,7 +40,7 @@ let DetectResult = (() => {
                 (Gameboard.table[0][i] === 'X' && Gameboard.table[1][i] === 'X' && Gameboard.table[2][i] === 'X') || // column check
                 (Gameboard.table[0][0] === 'X' && Gameboard.table[1][1] === 'X' && Gameboard.table[2][2] === 'X') || // diagonal check
                 (Gameboard.table[0][2] === 'X' && Gameboard.table[1][1] === 'X' && Gameboard.table[2][0] === 'X')) {
-                winner = 'Player 1';
+                winner = player1NameInput;
             }
         }
         for (let i = 0; i < 3; i++) {  // for Player 2(O)
@@ -24,7 +48,7 @@ let DetectResult = (() => {
                 (Gameboard.table[0][i] === 'O' && Gameboard.table[1][i] === 'O' && Gameboard.table[2][i] === 'O') || // column check
                 (Gameboard.table[0][0] === 'O' && Gameboard.table[1][1] === 'O' && Gameboard.table[2][2] === 'O') || // diagonal check
                 (Gameboard.table[0][2] === 'O' && Gameboard.table[1][1] === 'O' && Gameboard.table[2][0] === 'O')) {
-                winner = 'Player 2';
+                winner = player2NameInput;
             }
         }
         return winner;
@@ -55,10 +79,10 @@ let DetectResult = (() => {
 let handleGameResult = () => {
     let resultMessage = document.querySelector('.result-message')
     const result = DetectResult.checkWinner();
-    if (result === 'Player 1') {
-        resultMessage.textContent = 'Player 1 Won!'
-    } else if (result === 'Player 2') {
-        resultMessage.textContent = 'Player 2 Won!'
+    if (result === player1NameInput) {
+        resultMessage.textContent = `${player1NameInput.value} is the winner!`
+    } else if (result === player2NameInput) {
+        resultMessage.textContent = `${player2NameInput.value} is the winner!`
 
     } else if (DetectResult.checkDraw()) {
         resultMessage.textContent = 'Draw!'
@@ -101,7 +125,6 @@ let resetGame = (() => {
 
     replayButton.addEventListener('click', () => {
         gameEnded = false;
-
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 Gameboard.table[i][j] = '';
@@ -114,19 +137,3 @@ let resetGame = (() => {
         resultMessage.textContent = '';
     })
 })()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
